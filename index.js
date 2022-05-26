04ea0b1bba2986fc9367ef68fac4149efc17b1e5
Sam Welsh <sammwelsh@ymail.com>
2022-05-26 04:53:11 -0700

    Initial commit, copied WebSocketServer

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.disable('x-powered-by')
+
+app.use(express.static(__dirname + '/public'))
+
+require('./lib/wsserver.js')
+
+console.log()
+
+app.set('port', process.env.PORT || 3000)
+app.listen(
+    app.get('port'), 
+    function onAppListening() {
+        console.log(
+            '          ' + 
+            'Express Listening: ', 
+            app.get('port')
+        )
+    }
+)
